deb15a716c91d3511a954ab0493ba74d1aa1a4c2
Bar Nuri <barnuri@hotmail.com>
2024-11-18 16:06:44 +0200

    ToDictonaryList in results

--- CSharp.OpenSource.LinqToKql/Models/KustoQueryResultV1.cs
+++ CSharp.OpenSource.LinqToKql/Models/KustoQueryResultV1.cs
@@ -3,4 +3,31 @@
 public class KustoQueryResultV1
 {
     public List<KustoQueryResultV1Table> Tables { get; set; }
+
+    public List<Dictionary<string, object?>> ToDictonaryList()
+    {
+        var resultList = new List<Dictionary<string, object?>>();
+
+        if (Tables == null || Tables.Count == 0)
+        {
+            return resultList;
+        }
+
+        // only the first table is the results, other tables are metadata
+        var table = Tables[0];
+        var columns = table.Columns.Select(c => c.ColumnName).ToList();
+        var rows = table.Rows;
+
+        foreach (var row in rows)
+        {
+            var rowDict = new Dictionary<string, object?>();
+            for (var i = 0; i < columns.Count; i++)
+            {
+                rowDict[columns[i]] = row[i];
+            }
+            resultList.Add(rowDict);
+        }
+
+        return resultList;
+    }
 }
--- CSharp.OpenSource.LinqToKql/Models/KustoQueryResultV2.cs
+++ CSharp.OpenSource.LinqToKql/Models/KustoQueryResultV2.cs
@@ -2,4 +2,33 @@
 
 public class KustoQueryResultV2 : List<KustoQueryResultV2Item>
 {
+    public List<Dictionary<string, object?>> ToDictonaryList()
+    {
+        var resultList = new List<Dictionary<string, object?>>();
+        if (this == null || Count == 0)
+        {
+            return resultList;
+        }
+        var resultTable = this.FirstOrDefault(x => x.TableName == "PrimaryResult");
+        if (resultTable == null)
+        {
+            return resultList;
+        }
+
+        // only the first table is the results, other tables are metadata
+        var columns = resultTable.Columns.Select(c => c.ColumnName).ToList();
+        var rows = resultTable.Rows;
+
+        foreach (var row in rows)
+        {
+            var rowDict = new Dictionary<string, object?>();
+            for (var i = 0; i < columns.Count; i++)
+            {
+                rowDict[columns[i]] = row[i];
+            }
+            resultList.Add(rowDict);
+        }
+
+        return resultList;
+    }
 }
