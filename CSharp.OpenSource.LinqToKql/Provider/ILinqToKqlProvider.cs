c7430b7d6924464e701dcc0d6e03844e3e683891
Bar Nuri <barnuri@hotmail.com>
2024-11-24 11:06:43 +0200

    Update ILinqToKqlProvider.cs

--- CSharp.OpenSource.LinqToKql/Provider/ILinqToKqlProvider.cs
+++ CSharp.OpenSource.LinqToKql/Provider/ILinqToKqlProvider.cs
@@ -1,3 +1,9 @@
+﻿using CSharp.OpenSource.LinqToKql.Translator;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace CSharp.OpenSource.LinqToKql.Provider;
+
 public interface ILinqToKqlProvider<T> : IQueryable<T>, IQueryProvider, IOrderedQueryable<T>, IAsyncEnumerable<T>
 {
 }
