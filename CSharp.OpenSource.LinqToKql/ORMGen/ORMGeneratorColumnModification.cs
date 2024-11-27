f80ed91ad21a7d8b856718617ac81cc804be8fc3
Bar Nuri <barnuri@hotmail.com>
2024-11-27 15:08:21 +0200

    fix null refs

--- CSharp.OpenSource.LinqToKql/ORMGen/ORMGeneratorColumnModification.cs
+++ CSharp.OpenSource.LinqToKql/ORMGen/ORMGeneratorColumnModification.cs
@@ -2,9 +2,9 @@
 
 public class ORMGeneratorColumnModification
 {
-    public List<string> DatabasePatterns { get; set; }
-    public List<string> TableOrFunctionPatterns { get; set; }
-    public List<string> ColumnNamePatterns { get; set; }
+    public List<string> DatabasePatterns { get; set; } = new();
+    public List<string> TableOrFunctionPatterns { get; set; } = = new();
+    public List<string> ColumnNamePatterns { get; set; } = new();
     public string NewColumnType { get; set; }
     public string ColumnAttributes { get; set; }
     public bool Exclude { get; set; }
--- CSharp.OpenSource.LinqToKql/ORMGen/ORMGeneratorTableOrFunctionModification.cs
+++ CSharp.OpenSource.LinqToKql/ORMGen/ORMGeneratorTableOrFunctionModification.cs
@@ -3,8 +3,8 @@ namespace CSharp.OpenSource.LinqToKql.ORMGen;
 
 public class ORMGeneratorTableOrFunctionModification
 {
-    public List<string> DatabasePatterns { get; set; }
-    public List<string> TableOrFunctionPatterns { get; set; }
+    public List<string> DatabasePatterns { get; set; } = new();
+    public List<string> TableOrFunctionPatterns { get; set; } = new();
     public string ClassAttributes { get; set; }
     public string ClassInherit { get; set; }
     public List<string> BodyExtraLines { get; set; } = new();
