1970c0b0f699aa6decced7b2517f701dda34a192
Bar Nuri <barnuri@hotmail.com>
2024-12-17 15:24:48 +0200

    demo cluster

--- DemoCluster/run.sh
+++ DemoCluster/run.sh
@@ -1,1 +1,1 @@
-docker build -t kusto . && docker rm -f kusto && docker run -it --name kusto -p 8080:8080 kusto
+docker build -t kusto . && docker rm -f kusto && docker run -d -it --name kusto -p 8080:8080 kusto
