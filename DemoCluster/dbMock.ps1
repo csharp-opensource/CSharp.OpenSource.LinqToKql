5c7f1e7c069a8f2c55c8377578933626d35298f0
Bar Nuri <barnuri@hotmail.com>
2025-01-06 11:40:50 +0200

    add-ingested-data

--- DemoCluster/dbMock.ps1
+++ DemoCluster/dbMock.ps1
@@ -34,3 +34,9 @@ req @{
     db="TestDatabase1"
     csl=".create table SampleTable ( Id: int, Name: string, Date: datetime, DateOnly: date, TimeOnly: timespan, Time: timespan, IsActive: bool, Year: int, Description: string, Type: string, Value: long, Numbers: dynamic, Nested: dynamic, prop_newtonsoft: long, prop_text_json: long, prop_data_member: long)"
 }
+req @{
+    db="TestDatabase1"
+    csl=".ingest inline into table SampleTable <|
+1,Active Item,2025-01-06T12:00:00Z,2025-01-06,12:00:00,12:00:00,true,2025,This is an active item,TypeA,100,`"[1,2,3]`",`"{\`"key1\`":\`"value1\`",\`"key2\`":\`"value2\`"}`",10,20,30
+2,Inactive Item,2025-01-06T15:30:00Z,2025-01-06,15:30:00,15:30:00,false,2025,This is an inactive item,TypeB,200,`"[4,5,6]`",`"{\`"key1\`":\`"value3\`",\`"key2\`":\`"value4\`"}`",40,50,60"
+}
